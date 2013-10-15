95ebc53581143520efb24d06fcd22e464e4eb488
Juan Tareq Gonzalez <alu0100603866@ull.edu.es>
2013-10-15 14:56:28 +0100

    Realizado test y programa

--- racional.rb
+++ racional.rb
@@ -4,18 +4,50 @@
 require "./gcd.rb"
 
 class Fraccion
+	attr_reader :n, :d
 	def initialize(num, den) 
-		@n = num
-		@d = den
-		@max = gcd(num, den)
-		@n = @n/@max
-		@d = @d/@max
+		max = gcd(num, den)	
+		#teniendo el maximo comun divisor operamos y los añadimos
+		@n = num/max		
+		@d = den/max
 	end
 
 	def to_s
-		"Fraccion: #{@n} / #{@d} maximo comun divisor #{@max} "
+		"Fraccion: #{@n} / #{@d} "
+	end
+
+	def suma(x)  #x es un objeto de la clase fraccion
+		@n = @n*x.d + x.n * @d
+		@d = @d * x.d
+		#una vez realizada la suma la reducimos
+		#a su minima expresion
+		max = gcd(@n, @d)
+		@n = @n / max
+		@d = @d / max
+	end
+
+	def resta(x)
+		@n = @n*x.d - x.n * @d
+		@d = @d * x.d
+		max = gcd(@n, @d)
+		@n = @n / max
+		@d = @d / max
+	end
+	
+	def producto(x)
+		@n = @n * x.n
+		@d = @d * x.d
+		max = gcd(@n, @d)
+		@n = @n / max
+		@d = @d / max
+	end
+
+	def division(x)
+		@n = @n * x.d
+		@d = @d * x.n
+		max = gcd(@n, @d)
+		@n = @n / max
+		@d = @d / max
 	end
 end
 
-fraccion = Fraccion.new(9,3)
-puts fraccion
--- tc_racional.rb
+++ tc_racional.rb
@@ -1,8 +1,42 @@
+#! /usr/bin/ruby
 # Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
 
 require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+	def test_suma
+		@p1 = Fraccion.new(3,4)
+		@p2 = Fraccion.new(7,8)
+		@p1.suma(@p2)
+		@psuma = Fraccion.new(13,8)
+		#comprobamos que la suma sea igual al resultado
+		#que obtenemos operando en papel
+		assert_not_same(@p1,@psuma)
+	end
+
+	def test_resta
+		@p1 = Fraccion.new(13, 8)
+		@p2 = Fraccion.new(7,8)
+		@p1.resta(@p2)
+		@presta = Fraccion.new(3,4)
+		assert_not_same(@p1, @presta)
+	end
+
+	def test_producto
+		@p1 = Fraccion.new(3, 8)
+                @p2 = Fraccion.new(5,2)
+                @p1.producto(@p2)
+                @pprod = Fraccion.new(15,16)
+		assert_not_same(@p1, @pprod)
+	end
+
+	def test_division
+		@p1 = Fraccion.new(3, 5)
+                @p2 = Fraccion.new(4,2)
+                @p1.division(@p2)
+                @pdiv = Fraccion.new(6,20)
+		assert_not_same(@p1, @pdiv)
+	end
 
 end
